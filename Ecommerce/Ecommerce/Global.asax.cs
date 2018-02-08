9094786b07670f608a49b47adfa03702afb77d49
Seung Chi <seungyoonchi@gmail.com>
2018-02-08 15:25:42 -0800

    Modify default.aspx & site.master

--- Ecommerce/Ecommerce/Global.asax.cs
+++ Ecommerce/Ecommerce/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Data.Entity;
+using Ecommerce.Models;
 
 namespace Ecommerce
 {
@@ -16,6 +18,9 @@ namespace Ecommerce
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize the product database.
+            Database.SetInitializer(new ProductDatabaseInitializer());
         }
     }
 }
