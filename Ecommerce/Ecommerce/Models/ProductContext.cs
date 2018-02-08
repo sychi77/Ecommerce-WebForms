1598e5ff38c98c7bc0b27597dcf87a9dc8dc5e68
Seung Chi <seungyoonchi@gmail.com>
2018-02-08 14:58:45 -0800

    Initialize solution

---
+++ Ecommerce/Ecommerce/Models/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace Ecommerce.Models
+{
+    public class ProductContext: DbContext
+    {
+        public ProductContext() : base("Ecommerce")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
